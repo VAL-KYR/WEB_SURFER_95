27d0ff5f92f86fa19bb0ab51fb81431320ef5cc1
Valerie T. Heyerdahl <c418fan@gmail.com>
2018-03-24 01:48:52 -0400

    Basic Player Movement

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[System.Serializable]
+public class player : MonoBehaviour {
+
+
+
+    // Debug Management
+    [System.Serializable]
+    public class mDebug
+    {
+        public bool gizmos = false;
+    }
+    public mDebug debug = new mDebug();
+
+    // Movement Management
+    [System.Serializable]
+    public class Movement
+    {
+        public float speed = 0.5f;
+    }
+    public Movement movement = new Movement();
+
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // limit movement HERE /////////////////////////////////////////////////
+        
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            Debug.Log(Input.GetAxis("Horizontal"));
+            transform.position = new Vector3(movement.speed * Input.GetAxis("Horizontal") + transform.position.x, transform.position.y, transform.position.z);
+        }
+		
+	}
+}
