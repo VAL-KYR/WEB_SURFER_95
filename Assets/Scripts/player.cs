cffffbce1f99a2efca46c89875aeadfa0563ec16
Valerie T. Heyerdahl <c418fan@gmail.com>
2018-03-24 22:30:00 -0400

    Basic Movement Smoothed

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -12,6 +12,8 @@ public class player : MonoBehaviour {
     [System.Serializable]
     public class mDebug
     {
+        public bool rawInput = false;
+        public bool modInput = false;
         public bool gizmos = false;
     }
     public mDebug debug = new mDebug();
@@ -20,7 +22,11 @@ public class player : MonoBehaviour {
     [System.Serializable]
     public class Movement
     {
+        public bool smoothSpeed = false;
+        public bool lurchSpeed = false;
+        public float smoothAccel = 1.8f;
         public float speed = 0.5f;
+        public float boundRange = 5f;
     }
     public Movement movement = new Movement();
 
@@ -34,14 +40,59 @@ public class player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-        // limit movement HERE /////////////////////////////////////////////////
         
+        // Side to Side Movement 
         if (Input.GetAxis("Horizontal") != 0)
         {
-            Debug.Log(Input.GetAxis("Horizontal"));
-            transform.position = new Vector3(movement.speed * Input.GetAxis("Horizontal") + transform.position.x, transform.position.y, transform.position.z);
+
+            if (debug.rawInput)
+                Debug.Log("Raw input value: " + Input.GetAxis("Horizontal"));
+
+
+            if ((Input.GetAxis("Horizontal") > 0 && transform.position.x < movement.boundRange))
+            {
+                transform.position = new Vector3(movement.speed * inputRecurve(Mathf.Abs(Input.GetAxis("Horizontal"))) + transform.position.x,
+                                                transform.position.y,
+                                                transform.position.z);
+            }
+
+            if ((Input.GetAxis("Horizontal") < 0 && transform.position.x > -movement.boundRange))
+            {
+                transform.position = new Vector3(-movement.speed * inputRecurve(Mathf.Abs(Input.GetAxis("Horizontal"))) + transform.position.x,
+                                                transform.position.y,
+                                                transform.position.z);
+            }
+            
         }
+
+
 		
 	}
+
+    private void OnDrawGizmos()
+    {
+        if (debug.gizmos)
+        {
+            // Camera look direction
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawRay(GameObject.Find("CenterEyeAnchor").transform.position, GameObject.Find("CenterEyeAnchor").transform.forward * 100f);
+        }
+    }
+
+    float inputRecurve(float inputSignal)
+    {
+        // convert raw input with curve
+        float returnSignal;
+        if (movement.smoothSpeed)
+            returnSignal =  Mathf.Pow(inputSignal, movement.smoothAccel/1.0f);
+        else if (movement.lurchSpeed)
+            returnSignal = Mathf.Pow(inputSignal, 1.0f / movement.smoothAccel);
+        else
+            returnSignal = inputSignal;
+
+        if (debug.modInput)
+            Debug.Log("Modified input value: " + returnSignal);
+
+        return returnSignal;
+    }
 }
