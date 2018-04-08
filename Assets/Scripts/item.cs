d1d34d1438633cd6577a38e7437d48f2429c7125
Valerie T. Heyerdahl <c418fan@gmail.com>
2018-04-08 15:10:26 -0400

    Item Pickups Work

---
+++ Assets/Scripts/item.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<track>().itemEffects(gameObject.tag);
+            Destroy(gameObject);
+        }
+    }
+}
